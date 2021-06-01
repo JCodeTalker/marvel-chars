865dd09b716c90820084498bad54e38458e9e399
Jones <jonesoaresdasilva@gmail.com>
2021-06-01 17:49:23 -0300

    correcoes

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 
 var charNum = 0 //represents character currently on screen
 var json
-const timestamp = "1622316257"
+var timestamp = "1622316257"
 const publicapikey = '48aff73d463883e712ecdfb3ce8b6b0a'
-const md5 = '51b792a62058de22abd327f1947adf1f'
+var md5 = '51b792a62058de22abd327f1947adf1f'
 
 fetch(`https://gateway.marvel.com/v1/public/characters?limit=99&ts=${timestamp}&apikey=${publicapikey}&hash=${md5}`
 ).then((response) => {
     return response.json()
 }).then((jsonParsed) => {
     json = jsonParsed
-    console.log(json)
     loadCharacter(charNum, json)
 })
-
 
 function navBar() {
     document.getElementById('current').innerHTML = charNum + 1
@@ -43,15 +41,15 @@ function navBar() {
     charNum >= 1 ? minusOne.style = "display: block;" : minusOne.style = "display: none;"
 }
 
-
 function loadCharacter(number, jsonParsed) {
     const herois = document.getElementById('heros')
     const name = jsonParsed.data.results[number].name
-    var description = jsonParsed.data.results[number].description
+    const description = jsonParsed.data.results[number].description
+    const descriptionElem = document.getElementById('description')
     if (description) {
-        document.getElementById('description').innerHTML = description
+        descriptionElem.innerHTML = description
     } else {
-        document.getElementById('description').innerHTML = 'No description found.'
+        descriptionElem.innerHTML = 'No description found.'
     }
     navBar()
     document.getElementById('name').innerHTML = `Name: ${name}`
